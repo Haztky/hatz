6c8c8a76dd5265029c298fef289c71a4d89c2e47
zXiro1 <prince.martinez@edu.sait.ca>
2024-12-11 03:50:58 -0700

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
-    },
-  };
-  
-  export default nextConfig;
+  env: {
+    DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'zlib-sync': false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
